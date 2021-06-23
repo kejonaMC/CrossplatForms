05972fe133cd67dc173a07be8017da62ffd50ac2
Konicai <71294714+Konicai@users.noreply.github.com>
2021-06-23 16:40:02 -0400

    add command to make other players open a form

--- src/main/java/dev/projectg/geyserhub/SelectorLogger.java
+++ src/main/java/dev/projectg/geyserhub/SelectorLogger.java
@@ -20,6 +20,23 @@ public class SelectorLogger implements Reloadable {
         ReloadableRegistry.registerReloadable(this);
     }
 
+    public void log(Level level, String message) {
+        switch (level) {
+            default: // intentional fallthrough
+            case INFO:
+                info(message);
+                break;
+            case WARN:
+                warn(message);
+                break;
+            case SEVERE:
+                severe(message);
+                break;
+            case DEBUG:
+                debug(message);
+                break;
+        }
+    }
     public void info(String message) {
         plugin.getLogger().info(message);
     }
@@ -33,6 +50,13 @@ public class SelectorLogger implements Reloadable {
         if (debug) {
             plugin.getLogger().info(message);
         }
+    }
+
+    public enum Level {
+        INFO,
+        WARN,
+        SEVERE,
+        DEBUG
     }
 
     public boolean isDebug() {
--- src/main/java/dev/projectg/geyserhub/command/GeyserHubCommand.java
+++ src/main/java/dev/projectg/geyserhub/command/GeyserHubCommand.java
@@ -1,9 +1,12 @@
 package dev.projectg.geyserhub.command;
 
+import dev.projectg.geyserhub.SelectorLogger;
 import dev.projectg.geyserhub.config.ConfigId;
 import dev.projectg.geyserhub.reloadable.ReloadableRegistry;
 import dev.projectg.geyserhub.module.menu.bedrock.BedrockFormRegistry;
 import dev.projectg.geyserhub.module.menu.java.JavaMenu;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -13,6 +16,8 @@ import org.geysermc.floodgate.api.FloodgateApi;
 import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nonnull;
+import javax.management.ObjectName;
+import java.util.Objects;
 import java.util.UUID;
 
 public class GeyserHubCommand implements CommandExecutor {
@@ -20,12 +25,13 @@ public class GeyserHubCommand implements CommandExecutor {
     private static final String[] HELP = {
             "/ghub - Opens the default form if one exists. If not, shows the help page",
             "/ghub - Opens the help page",
-            "/ghub <form> - Sends the player a selector with the defined name",
+            "/ghub form <form> - Open a form with the defined name",
+            "/ghub form <form> <player> - Sends a form to a given player",
             "/ghub reload - reloads the selector"
     };
 
-    private static final String NO_PERMISSION = "[GeyserHub] Sorry, you don't have permission to run that command!";
-    private static final String UNKNOWN = "[GeyserHub] Sorry, that's an unknown command!";
+    private static final String NO_PERMISSION = "Sorry, you don't have permission to run that command!";
+    private static final String UNKNOWN = "Sorry, that's an unknown command!";
 
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
@@ -44,12 +50,12 @@ public class GeyserHubCommand implements CommandExecutor {
             case "reload":
                 if (commandSender.hasPermission("geyserhub.reload")) {
                     if (ReloadableRegistry.reloadAll()) {
-                        commandSender.sendMessage("[GeyserHub] Successfully reloaded.");
+                        sendMessage(commandSender, SelectorLogger.Level.INFO, "Successfully reloaded.");
                     } else {
-                        commandSender.sendMessage("[GeyserHub] There was an error reloading something! Please check the server console for further information.");
+                        sendMessage(commandSender, SelectorLogger.Level.SEVERE, "There was an error reloading something! Please check the server console for further information.");
                     }
                 } else {
-                    commandSender.sendMessage(NO_PERMISSION);
+                    sendMessage(commandSender, SelectorLogger.Level.SEVERE, NO_PERMISSION);
                 }
                 break;
             case "help":
@@ -58,18 +64,30 @@ public class GeyserHubCommand implements CommandExecutor {
             case "form":
                 if (commandSender.hasPermission("geyserhub.form")) {
                     if (args.length == 1) {
-                        commandSender.sendMessage("[GeyserHub] Please specify a form to open! Specify a form with \"/ghub form <form>\"");
-                    } else if (args.length > 2) {
-                        commandSender.sendMessage("[GeyserHub] This command only takes one argument!");
-                    } else {
+                        sendMessage(commandSender, SelectorLogger.Level.SEVERE, "Please specify a form to open! Specify a form with \"/ghub form <form>\"");
+                    } else if (args.length == 2) {
                         sendForm(commandSender, args[1]);
+                    } else if (args.length == 3) {
+                        if (commandSender.hasPermission("geyserhub.form.others")) {
+                            Player target = Bukkit.getServer().getPlayer(args[2]);
+                            if (target == null) {
+                                sendMessage(commandSender, SelectorLogger.Level.SEVERE, "That player doesn't exist!");
+                            } else {
+                                sendForm(target, args[1]);
+                                sendMessage(commandSender, SelectorLogger.Level.INFO, "Made " + target.getName() + " open form: " + args[1]);
+                            }
+                        } else {
+                            sendMessage(commandSender, SelectorLogger.Level.SEVERE, NO_PERMISSION);
+                        }
+                    } else {
+                        sendMessage(commandSender, SelectorLogger.Level.SEVERE, "Too many command arguments!");
                     }
                 } else {
-                    commandSender.sendMessage(NO_PERMISSION);
+                    sendMessage(commandSender, SelectorLogger.Level.SEVERE, NO_PERMISSION);
                 }
                 break;
             default:
-                commandSender.sendMessage(UNKNOWN);
+                sendMessage(commandSender, SelectorLogger.Level.SEVERE, UNKNOWN);
                 break;
         }
         return true;
@@ -96,16 +114,41 @@ public class GeyserHubCommand implements CommandExecutor {
                     if (BedrockFormRegistry.getInstance().getFormNames().contains(formName)) {
                         BedrockFormRegistry.getInstance().sendForm(FloodgateApi.getInstance().getPlayer(uuid), formName);
                     } else {
-                        player.sendMessage("Sorry, that form doesn't exist! Specify a form with \"/ghub form <form>\"");
+                        sendMessage(player, SelectorLogger.Level.SEVERE, "Sorry, that form doesn't exist! Specify a form with \"/ghub form <form>\"");
                     }
                 } else {
-                    player.sendMessage("Sorry, Bedrock forms are disabled!");
+                    sendMessage(player, SelectorLogger.Level.SEVERE, "Sorry, Bedrock forms are disabled!");
                 }
             } else {
                 JavaMenu.openMenu(player, ConfigId.SELECTOR);
             }
         } else if (commandSender instanceof ConsoleCommandSender) {
             sendHelp(commandSender);
+        }
+    }
+
+    private void sendMessage(@Nonnull CommandSender sender, @Nonnull SelectorLogger.Level level, @Nonnull String message) {
+        Objects.requireNonNull(sender);
+        Objects.requireNonNull(level);
+        Objects.requireNonNull(message);
+
+        if (sender instanceof ConsoleCommandSender) {
+            SelectorLogger.getLogger().log(level, message);
+        } else {
+            ChatColor colour;
+            switch (level) {
+                default: // intentional fallthrough
+                case INFO:
+                    colour = ChatColor.RESET;
+                    break;
+                case WARN:
+                    colour = ChatColor.GOLD;
+                    break;
+                case SEVERE:
+                    colour = ChatColor.RED;
+                    break;
+            }
+            sender.sendMessage("[GeyserHub] " + colour + message);
         }
     }
 }
