7dd3d9fb32520c1c2c5b6764617c18467b7beaf6
Konicai <71294714+Konicai@users.noreply.github.com>
2021-11-25 17:16:39 -0500

    update config version in 4-5 main updater

--- src/main/java/dev/projectg/geyserhub/config/updaters/MAIN_5.java
+++ src/main/java/dev/projectg/geyserhub/config/updaters/MAIN_5.java
@@ -27,6 +27,7 @@ public class MAIN_5 implements ConfigUpdater {
             teleporter.set("Yaw", 0); // Default
 
             config.set("Join-Teleporter.Coordinates", null); // Remove
+            config.set("Config-Version", 5);
 
             return true;
         } catch (NumberFormatException e) {
