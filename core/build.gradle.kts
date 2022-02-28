068de10a27a16e33f70008cfee3a416ce2555164
Konicai <71294714+Konicai@users.noreply.github.com>
2022-02-28 18:55:15 -0500

    Dependency updates for 1.18.2 (#45)
    * Bump cloud-minecraft-extras from 1.6.1 to 1.6.2
    * Bump commodore from 1.12 to 1.13
    * Bump cloud-paper from 1.6.1 to 1.6.2
    * Bump cloud-core from 1.6.1 to 1.6.2
    * Bump adventure-platform-bukkit from 4.0.1 to 4.1.0
    * bump geyser from 2.0.1 to 2.0.2
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -9,13 +9,13 @@ dependencies {
     testImplementation("org.geysermc.cumulus:cumulus:1.0-SNAPSHOT") // needed for testing button components
     testImplementation("com.google.code.gson:gson:2.8.6") // needed for cumulus
 
-    compileOnly("org.geysermc:geyser-api:2.0.1-SNAPSHOT")
-    compileOnly("org.geysermc:core:2.0.1-SNAPSHOT") {
+    compileOnly("org.geysermc:geyser-api:2.0.2-SNAPSHOT")
+    compileOnly("org.geysermc:core:2.0.2-SNAPSHOT") {
         isTransitive = false // exclude all the junk we won't and can't use
     }
     compileOnly("org.geysermc.floodgate:api:2.1.0-SNAPSHOT")
-    api("cloud.commandframework:cloud-core:1.6.1")
-    api("cloud.commandframework:cloud-minecraft-extras:1.6.1")
+    api("cloud.commandframework:cloud-core:1.6.2")
+    api("cloud.commandframework:cloud-minecraft-extras:1.6.2")
     api("net.kyori:adventure-api:4.9.3")
     api("net.kyori:adventure-text-serializer-legacy:4.9.3")
     api("org.spongepowered:configurate-yaml:4.1.2")
