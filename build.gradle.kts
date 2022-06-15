b8b35a2dbd4cdab8f2f04068fc4d5100f8b9533d
Konicai <71294714+Konicai@users.noreply.github.com>
2022-06-15 15:46:15 -0400

    Bump to 1.2.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ allprojects{
     apply(plugin = "net.kyori.indra.git")
 
     group = "dev.projectg"
-    version = "1.2.0"
+    version = "1.2.1"
 
     tasks.withType<JavaCompile> {
         options.encoding = "UTF-8"
