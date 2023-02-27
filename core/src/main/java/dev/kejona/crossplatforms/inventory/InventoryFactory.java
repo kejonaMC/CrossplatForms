94b3e487496b4cb49cb944999c07eac246842dfa
Konicai <71294714+Konicai@users.noreply.github.com>
2023-02-26 20:21:37 -0500

    Remove comment in InventoryFactory

--- core/src/main/java/dev/kejona/crossplatforms/inventory/InventoryFactory.java
+++ core/src/main/java/dev/kejona/crossplatforms/inventory/InventoryFactory.java
@@ -7,12 +7,6 @@ import javax.annotation.Nullable;
 import java.util.List;
 
 public interface InventoryFactory {
-    /*
-    todo: This could be generified, along with config classes. this would allow for Configurate to directly serialize
-    implementation-specific classes, such as material enums. Additionally, if Interface#openInventory was moved to this
-    interface, then the usage of generics would mean that implementations would not have to cast Inventory/Item to get
-    their implementation back
-     */
 
     InventoryHandle chest(String title, int chestSize);
     InventoryHandle inventory(String title, InventoryLayout layout);
