dc5d2ce76872cef88b644301ac6c324ec990cafd
Konicai <71294714+Konicai@users.noreply.github.com>
2023-02-26 20:31:37 -0500

    Remove InventoryFiller.java

--- core/src/main/java/dev/kejona/crossplatforms/filler/UniversalFiller.java
+++ core/src/main/java/dev/kejona/crossplatforms/filler/UniversalFiller.java
@@ -3,7 +3,6 @@ package dev.kejona.crossplatforms.filler;
 import dev.kejona.crossplatforms.context.PlayerContext;
 import dev.kejona.crossplatforms.interfacing.bedrock.custom.Option;
 import dev.kejona.crossplatforms.interfacing.bedrock.simple.SimpleButton;
-import dev.kejona.crossplatforms.interfacing.java.ItemButton;
 import lombok.Getter;
 import lombok.experimental.Accessors;
 import org.spongepowered.configurate.objectmapping.ConfigSerializable;
@@ -11,13 +10,12 @@ import org.spongepowered.configurate.objectmapping.meta.Setting;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import java.util.Map;
 import java.util.stream.Stream;
 
 @Getter
 @Accessors(fluent = true)
 @ConfigSerializable
-public abstract class UniversalFiller implements OptionFiller, SimpleFormFiller, InventoryFiller {
+public abstract class UniversalFiller implements OptionFiller, SimpleFormFiller {
 
     /**
      * OptionFormat is used here since Option is deserialized as a scalar, and the format node is a map.
@@ -34,22 +32,12 @@ public abstract class UniversalFiller implements OptionFiller, SimpleFormFiller,
     private int insertIndex = -1;
 
     @Nullable
-    @Setting(value = "format")
-    private ItemButton itemTemplate;
-
-    @Nullable
     @Override
     public Option optionTemplate() {
         if (optionTemplate != null) {
             return optionTemplate.text;
         }
         return null;
-    }
-
-    @Nonnull
-    @Override
-    public Map<Integer, ItemButton> rawItems(PlayerContext context) {
-        throw new AssertionError("Not yet implemented");
     }
 
     @Nonnull
